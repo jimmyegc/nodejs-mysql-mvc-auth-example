7b5c87bdece7bbd296b54324b1f0b38d1afeb69e
Jimmy García <jimmyegc@gmail.com>
2024-07-23 20:32:47 -0600

    proyecto inicial

---
+++ models/mUser.js
@@ -0,0 +1,33 @@
+import db from "../config/db.js";
+import bcrypt from "bcrypt";
+
+const mUser = {
+  create: async (user) => {
+    try {
+      const hash = await bcrypt.hash(user.password, 10);
+      const [results] = await db.query(
+        "INSERT INTO users (username, password) VALUES (?,?)",
+        [user.username, hash]
+      );
+      return results;
+    } catch (err) {
+      throw {
+        status: 500,
+        message: `Error al crear el usuario ${user.username}`,
+      };
+    }
+  },
+  getOne: async (username) => {
+    try {
+      const [results] = await db.query(
+        "SELECT * FROM users WHERE username = ?",
+        [username]
+      );
+      return results;
+    } catch (err) {
+      throw { status: 500, message: `Error al obtener el usuario ${username}` };
+    }
+  },
+};
+
+export default mUser;
